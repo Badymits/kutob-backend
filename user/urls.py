a4adbfac1c8ccfadd94486f771de777eb6001322
Elijah Cajucom <elijah.cajucom@gmail.com>
2024-11-03 19:03:48 +0800

    added update player url

--- user/urls.py
+++ user/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('register/', views.createUser, name='create-user'),
     path('delete/<str:username>/', views.deleteUser, name='delete-user'),
+    path('update-player/', views.updateUserSettings, name='update-player'),
+    path('user-data/', views.returnUserData, name='user-data')
 ]
