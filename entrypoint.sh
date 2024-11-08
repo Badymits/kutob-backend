80880e0c37598a91d2345b73eb5ce8a33c56aac1
Elijah Cajucom <elijah.cajucom@gmail.com>
2024-11-08 21:26:16 +0800

    added entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+python manage.py makemigrations
+python manage.py migrate --no-input
+python manage.py collectstatic --no-input
+
+# Start Gunicorn
+echo "Starting Gunicorn..."
+exec gunicorn kutob_backend.wsgi:application --bind 0.0.0.0:8000
