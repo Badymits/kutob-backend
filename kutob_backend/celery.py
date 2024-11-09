2f4c66ba2b11361f6ed6715c9352c70dad812c9d
Elijah Cajucom <elijah.cajucom@gmail.com>
2024-11-09 09:57:58 +0800

    added timezone

--- kutob_backend/celery.py
+++ kutob_backend/celery.py
@@ -6,6 +6,11 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kutob_backend.settings')
 
 app = Celery('kutob_backend')
 
+app.conf.update(
+    timezone = 'Asia/Manila',  # Correct timezone format
+    enable_utc = True,
+)
+
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # this will find tasks in the django project with the decorator "shared tasks"
