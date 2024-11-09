62d7cb367a0408ee815e4261d5b828b0270b0c46
Elijah Cajucom <elijah.cajucom@gmail.com>
2024-11-09 10:06:00 +0800

    added static root dir in settings. removed collecstatic command from entrypoint

--- kutob_backend/settings.py
+++ kutob_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 from celery.schedules import crontab
 from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -222,9 +223,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_ROOT = None
-
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
