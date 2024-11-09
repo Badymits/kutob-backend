acb8b1ff641f9f4add5ad691ca534f1f0fb58fe3
Elijah Cajucom <elijah.cajucom@gmail.com>
2024-11-09 11:34:00 +0800

    changed redis localtion cache

--- kutob_backend/settings.py
+++ kutob_backend/settings.py
@@ -124,7 +124,7 @@ CHANNEL_LAYERS = {
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/1',  # Use database 1
+        'LOCATION': 'redis://redis:6379/1',  # Use database 1
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
